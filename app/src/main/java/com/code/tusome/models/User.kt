ba91256566a5037c8db24bd1a71d1a77cb9a7656
Jamie Omondi <cruiseomondi90@gmail.com>
2022-11-20 03:08:31 +0300

    Added MainViewmodel

---
+++ app/src/main/java/com/code/tusome/models/User.kt
@@ -0,0 +1,3 @@
+package com.code.tusome.models
+
+data class User(val uid: String, val username: String,val  email: String,val imageUrl: String)
--- app/src/main/java/com/code/tusome/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/code/tusome/ui/viewmodels/MainViewModel.kt
@@ -1,7 +1,61 @@
 package com.code.tusome.ui.viewmodels
 
 import android.app.Application
+import android.net.Uri
+import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.code.tusome.models.User
+import com.code.tusome.utils.Utils
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.coroutines.launch
 
-class MainViewModel(application: Application):AndroidViewModel(application) {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+    private var loginStatus = false
+    private var registerStatus = false
+    fun login(email: String, password: String): Boolean {
+        viewModelScope.launch {
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener {
+                    loginStatus = true
+                }.addOnFailureListener {
+                    loginStatus = false
+                }
+        }
+        return loginStatus
+    }
+
+    fun register(
+        username: String,
+        email: String,
+        password: String,
+        imageUri: Uri,
+        view: View
+    ): Boolean {
+        viewModelScope.launch {
+            val ref = FirebaseStorage.getInstance().getReference("images")
+            ref.putFile(imageUri)
+                .addOnSuccessListener {
+                    ref.downloadUrl.addOnSuccessListener {
+                        val imageUrl = it.toString()
+                        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                            .addOnSuccessListener {
+                                val uid = FirebaseAuth.getInstance().currentUser!!.uid
+                                val user = User(uid, username, email, imageUrl)
+                                FirebaseDatabase.getInstance().getReference("users/$uid")
+                                    .setValue(user)
+                                    .addOnSuccessListener {
+                                        Utils.snackbar(view,"Registration successful, Login")
+                                    }
+                            }.addOnFailureListener { e ->
+                                Utils.snackbar(view, e.message!!)
+                            }
+                    }
+                }
+        }
+    }
+
 }
